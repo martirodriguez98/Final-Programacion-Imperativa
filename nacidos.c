c3b70137a6f172985dc7926153d47cd1b7c1fcbc
ggordyn <52581721+ggordyn@users.noreply.github.com>
2019-12-01 20:01:20 -0300

    comienzo main
    primera parte del main, hasta cargar las provincias

--- nacidos.c
+++ nacidos.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+int main(int argc, char **argv)
+{
+	if(argc!=3)
+	{
+		printf("Error en la cantidad de argumentos. Pasar como parámetros el archivo de provincias y el de nacimientos\n");
+		return 1;
+	}
+	provListADT prov=newProvList();
+	FILE *f=fopen(argv[1],"r"); //abro el archivo de provincias que recibi como paramatro
 
+	if(f==NULL)
+	{
+		printf("ERROR: No se pudo abrir el archivo\n");
+		return 1;
+	}
+
+	cagarProvincias(f,prov); //cargo las estaciones al TAD
+	fclose(f); //cuando termino de leer todas las provincias y agregarlas, cierro el archivo
+}
