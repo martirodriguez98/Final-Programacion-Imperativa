c14bbb6f8ac43a075dc97b638d22d207d387c934
ggordyn <52581721+ggordyn@users.noreply.github.com>
2019-12-01 23:01:45 -0300

    intento de cargarprovincias nuevo

--- library.c
+++ library.c
@@ -1,4 +1,4 @@
-#define MAX_LINELENGTH 100
+#define MAX_LINELENGTH 40
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,36 +7,14 @@
 #include "provListADT.h"
 
 void cargarProvincias(FILE * f, provListADT prov){
-	char * buf=malloc(MAX_LINELENGTH); //creo un vector buffer para almacenar cada linea
-	int cont=0;
-	int numCampo;
+	char format[]="%zu,%s\n"
+	size_t id;
+	char * name = malloc(MAX_LENGTH);
 
-	while(fgets(buf, MAX_LINELENGTH, f)){
-		numCampo=0;
-		cont++;
-
-		if(cont==1)
-			continue;	//me salteo el header que no quiero leer
-
-		char * campo = strtok(buf, ",\n"); //empiezo a recorrer la linea del CSV
-		size_t id;
-		char * name;
-		while(campo){
-			if(numCampo == 0){	//si leo el id lo guardo
-				id=atoi(campo);
-			}
-
-			if(numCampo == 1){	//si leo el nombre lo guardo
-				name=malloc(sizeof(campo));
-				strcpy(name, campo);
-			}
-
-			campo=strtok(NULL, ",\n");	//avanzo de campo
-			numCampo++;
-		}
+	while(fgetc(prov) != '\n'); //salteo el header
+	while(fscanf(prov, format, &id, name) == 2){
 		addProvince(prov, id, name);
-		free(name);
 	}
-	free(buf);
-}
 
+	free(name);
+}
