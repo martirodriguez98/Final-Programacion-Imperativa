ac31c72092ce160ab0957f6b62cea42e91d85aa0
ggordyn <52581721+ggordyn@users.noreply.github.com>
2019-12-04 22:44:17 -0300

    agregue descripciones de funciones

--- library.h
+++ library.h
@@ -4,7 +4,7 @@
 #include "provListADT.h"
 #include <stdlib.h>
 
-void query1(provListADT p);
-void query2(provListADT p);
-void query3(provListADT p);
+void query1(provListADT p); //realiza el query 1
+void query2(provListADT p); //realiza el query 2
+void query3(provListADT p); //realiza el query 3
 #endif
